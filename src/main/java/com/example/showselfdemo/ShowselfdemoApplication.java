83a08d9e280acebd3cd362ff6d127290e8ae2611
lingjianfei123 <1650548543@qq.com>
2023-06-04 17:01:03 +0800

    Initial commit

---
+++ src/main/java/com/example/showselfdemo/ShowselfdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.showselfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowselfdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowselfdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/showselfdemo/ShowselfdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.showselfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowselfdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
